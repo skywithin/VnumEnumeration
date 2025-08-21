c7a96ee86925b36cf4ea8c9bc01873829a4a859d
Max Vagner <sydneymax@gmail.com>
2025-08-22 06:47:31 +1000

    Initial commit

---
+++ src/VnumEnumeration/Vnum.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace VnumEnumeration;
+
+public abstract class Vnum<TEnum> : Vnum where TEnum : struct, Enum
+{
+    /// <summary>
+    /// Gets the enumeration value of the Vnum item.
+    /// </summary>
+    public TEnum Id => (TEnum)Enum.ToObject(typeof(TEnum), Value);
+
+    protected Vnum() : base() { }
+    protected Vnum(int value, string code) : base(value, code) { }
+    protected Vnum(TEnum value, string code) : base(Convert.ToInt32(value), code) { }
+}
+
+/// <summary>
+/// Provides a base class for creating enumeration-like constructs 
+/// with strongly-typed values and display codes.
+/// </summary>
+public abstract class Vnum : IEquatable<Vnum>
+{
+    /// <summary>
+    /// A thread-safe cache that stores arrays of <see cref="Vnum"/> instances for each derived type.
+    /// This cache is used to improve performance by avoiding repeated reflection operations when retrieving
+    /// all instances of a specific type (e.g., via <see cref="GetAll(Type)"/>). 
+    /// 
+    /// Once populated, subsequent calls for the same type will retrieve the cached array instead of 
+    /// invoking reflection, which can be computationally expensive. The cache uses a 
+    /// <see cref="ConcurrentDictionary{TKey, TValue}"/> to ensure thread safety in multi-threaded environments.
+    /// 
+    /// Note: This cache is static and internal, meaning it's shared across all instances of <see cref="Vnum"/>
+    /// and is not accessible outside the assembly.
+    /// </summary>
+    internal static readonly ConcurrentDictionary<Type, Array> _cache = new();
+
+    /// <summary>
+    /// Gets the numeric value of the Vnum item.
+    /// </summary>
+    public int Value { get; }
+
+    /// <summary>
+    /// Gets the string code of the Vnum item.
+    /// </summary>
+    public string Code { get; } = null!;
+
+    protected Vnum() { } // Required for reflection
+    protected Vnum(int value, string code)
+    {
+        Value = value;
+        Code = code ?? throw new ArgumentNullException(nameof(code));
+    }
+
+    /// <summary>
+    /// Returns the code of the Vnum item.
+    /// </summary>
+    public override string ToString() => Code;
+
+    /// <summary>
+    /// Retrieves all Vnum instances of a given type <typeparamref name="T"/>.
+    /// </summary>
+    public static IEnumerable<T> GetAll<T>(Func<T, bool>? predicate = null) where T : Vnum, new()
+    {
+        var all = GetAll(typeof(T)).Cast<T>();
+        return predicate is null ? all : all.Where(predicate);
+    }
+
+    /// <summary>
+    /// Retrieves all Vnum instances of the specified <see cref="Vnum"/> type.
+    /// </summary>
+    internal static Array GetAll(Type type)
+    {
+        //1. Validate the type parameter.
+        if (type is null)
+        {
+            throw new ArgumentNullException(
+                paramName: nameof(type),
+                message: "Type cannot be null");
+        }
+
+        if (!typeof(Vnum).IsAssignableFrom(type))
+        {
+            throw new ArgumentException(
+                message: $"Type '{type.Name}' is not a valid {nameof(Vnum)} type",
+                paramName: nameof(type));
+        }
+
+        //2. Check if the requested Vnum instances for type are already in _cache.
+        //3. If not cached, use reflection to get all public, static fields on type that represent Vnum instances.
+        //4. Store and return the results as an array.
+        return _cache.GetOrAdd(
+            key: type,
+            t => t.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)
+                  .Where(f => t.IsAssignableFrom(f.FieldType))
+                  .Select(f => f.GetValue(null)!)
+                  .ToArray());
+    }
+
+    /// <summary>
+    /// Retrieves a Vnum instance by its numeric value.
+    /// </summary>
+    public static T FromValue<T>(int value) where T : Vnum, new() =>
+        Parse<T, int>(
+            value: value,
+            description: nameof(value),
+            predicate: item => item.Value == value);
+
+    /// <summary>
+    /// Attempts to retrieve a Vnum instance by its numeric value.
+    /// </summary>
+    public static bool TryFromValue<T>(int value, out T vnum) where T : Vnum, new() =>
+        TryParse(() => FromValue<T>(value), out vnum);
+
+    /// <summary>
+    /// Retrieves a Vnum instance by its enum value.
+    /// </summary>
+    public static TVnum FromEnum<TVnum, TEnum>(TEnum value)
+        where TVnum : Vnum<TEnum>, new()
+        where TEnum : struct, Enum
+        => FromValue<TVnum>(Convert.ToInt32(value));
+
+    /// <summary>
+    /// Attempts to retrieve a Vnum instance by its enum value.
+    /// </summary>
+    public static bool TryFromEnum<TVnum, TEnum>(TEnum value, out TVnum vnum)
+        where TVnum : Vnum<TEnum>, new()
+        where TEnum : struct, Enum
+        => TryParse(() => FromValue<TVnum>(Convert.ToInt32(value)), out vnum);
+
+    /// <summary>
+    /// Retrieves a Vnum instance by its code.
+    /// </summary>
+    public static T FromCode<T>(string code, bool ignoreCase = false) where T : Vnum, new()
+    {
+        Func<T, bool> predicate =
+            (item) =>
+                ignoreCase
+                    ? item.Code.Equals(code, StringComparison.OrdinalIgnoreCase)
+                    : item.Code == code;
+
+        return Parse(
+            value: code,
+            description: nameof(code),
+            predicate: predicate);
+    }
+
+    /// <summary>
+    /// Attempts to retrieve a Vnum instance by its code.
+    /// </summary>
+    public static bool TryFromCode<T>(string code, bool ignoreCase, out T vnum) where T : Vnum, new() =>
+        TryParse(() => FromCode<T>(code, ignoreCase), out vnum);
+
+    /// <summary>
+    /// Attempts to retrieve a Vnum instance by its code (case sensitive).
+    /// </summary>
+    public static bool TryFromCode<T>(string code, out T vnum) where T : Vnum, new() =>
+        TryFromCode(code, ignoreCase: false, out vnum);
+
+    /// <summary>
+    /// Parses a Vnum instance based on a specified predicate.
+    /// This method is used internally to find a matching Vnum item.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Thrown if no matching Vnum instance is found.</exception>
+    private static T Parse<T, K>(
+        K value,
+        string description,
+        Func<T, bool> predicate) where T : Vnum, new()
+    {
+        var matchingItem = GetAll<T>().FirstOrDefault(predicate);
+
+        if (matchingItem is null)
+        {
+            throw new InvalidOperationException($"'{value}' is not a valid {description} in {typeof(T).Name}");
+        }
+
+        return matchingItem;
+    }
+
+    private static bool TryParse<T>(Func<T> parseFunc, out T result) where T : Vnum, new()
+    {
+        try
+        {
+            result = parseFunc();
+            return true;
+        }
+        catch (InvalidOperationException) // Narrow catch. Avoid masking catastrophic exceptions
+        {
+            result = null!;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Determines whether the specified object is equal to the current Vnum instance.
+    /// </summary>
+    public override bool Equals(object? obj) =>
+        obj is Vnum other &&
+        Equals(other);
+
+    /// <summary>
+    /// Determines whether the specified Vnum is equal to the current Vnum instance.
+    /// </summary>
+    public bool Equals(Vnum? other) =>
+        other != null &&
+        GetType().Equals(other.GetType()) &&
+        Value == other.Value;
+
+    /// <summary>
+    /// Returns the hash code for the Vnum item, based on its value.
+    /// </summary>
+    public override int GetHashCode() => HashCode.Combine(GetType(), Value);
+}
---
+++ tests/VnumEnumeration.Tests/VnumTests.cs
@@ -0,0 +1,452 @@
+﻿namespace VnumEnumeration.Tests;
+
+public class VnumTests
+{
+    [Fact]
+    public void Constructor_Should_Initialize_Properties_Correctly()
+    {
+        Assert.Equal(1, TestVnum1.OptionOne.Value);
+        Assert.Equal("OptionOne", TestVnum1.OptionOne.Code);
+
+        Assert.Equal(2, TestVnum1.OptionTwo.Value);
+        Assert.Equal("OptionTwo", TestVnum1.OptionTwo.Code);
+    }
+
+    [Fact]
+    public void ToString_Should_Return_Expected_Value()
+    {
+        Assert.Equal("OptionOne", TestVnum1.OptionOne.ToString());
+    }
+
+    [Fact]
+    public void Equals_Same_Instance_Should_Return_True()
+    {
+        // Arrange
+        var instance = TestVnum1.OptionOne;
+
+        // Act
+        Assert.True(instance.Equals(instance));
+    }
+
+    [Fact]
+    public void Equals_Different_Instances_With_Same_Value_And_Code_Should_Return_True()
+    {
+        // Arrange
+        var instance = TestVnum1.Stub(1, "OptionOne");
+
+        // Act
+        Assert.True(instance.Equals(TestVnum1.OptionOne));
+    }
+
+    [Fact]
+    public void Equals_Different_Instances_With_Same_Value_Different_Code_Should_Return_True()
+    {
+        // Arrange
+        var instance = TestVnum1.Stub(1, "BBBB");
+
+        // Act
+        Assert.True(instance.Equals(TestVnum1.OptionOne));
+    }
+
+    [Fact]
+    public void Equals_Different_Values_Should_Return_False()
+    {
+        Assert.False(TestVnum1.OptionOne.Equals(TestVnum1.OptionTwo));
+    }
+
+    [Fact]
+    public void Equals_Different_Instances_With_Same_Code_Different_Value_Should_Return_False()
+    {
+        // Arrange
+        var instance = TestVnum1.Stub(9999, "OptionOne");
+
+        // Act
+        Assert.False(instance.Equals(TestVnum1.OptionOne));
+    }
+
+    [Fact]
+    public void Equality_Operator_Should_Return_True_When_Values_Are_Equal()
+    {
+        // Act
+        var obj1 = TestVnum1.OptionOne;
+        var obj2 = TestVnum1.OptionOne;
+
+
+        var actual = obj1 == obj2;
+        // Assert
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void Equality_Operator_Should_Return_False_When_Values_Are_Not_Equal()
+    {
+        // Act
+        var actual = TestVnum1.OptionOne == TestVnum1.OptionTwo;
+        // Assert
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void Inequality_Operator_Should_Return_True_When_Values_Are_Not_Equal()
+    {
+        // Act
+        var actual = TestVnum1.OptionOne != TestVnum1.OptionTwo;
+        // Assert
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void Inequality_Operator_Should_Return_False_When_Values_Are_Equal()
+    {
+        // Arrange
+        var obj1 = TestVnum1.OptionOne;
+        var obj2 = TestVnum1.OptionOne;
+
+        // Act
+        var actual = obj1 != obj2;
+        // Assert
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void GetHashCode_Same_Value_Should_Be_Equal()
+    {
+        // Arrange
+        var instance = TestVnum1.Stub(1, "OptionOne");
+
+        // Act
+        Assert.Equal(instance.GetHashCode(), TestVnum1.OptionOne.GetHashCode());
+    }
+
+    [Fact]
+    public void FromValue_Given_Valid_Value_Should_Return_Correct_Vnum()
+    {
+        // Act
+        var actual = Vnum.FromValue<TestVnum1>(1);
+
+        // Assert 
+        Assert.Equal(TestVnum1.OptionOne, actual);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(9999)]
+    public void FromValue_Given_Invalid_Value_Should_Throw_Exception(int value)
+    {
+        // Act
+        var ex = Assert.Throws<InvalidOperationException>(() => Vnum.FromValue<TestVnum1>(value));
+
+        // Assert
+        Assert.Equal($"'{value}' is not a valid value in TestVnum1", ex.Message);
+    }
+
+    [Fact]
+    public void TryFromValue_Given_Valid_Value_Should_Return_True_With_Expected_Vnum()
+    {
+        // Act
+        var actual = Vnum.TryFromValue<TestVnum1>(2, out var vnum);
+
+        // Assert
+        Assert.True(actual);
+        Assert.Equal(TestVnum1.OptionTwo, vnum);
+    }
+
+    [Fact]
+    public void TryFromValue_Given_Invalid_Value_Should_Return_False_With_Null_Vnum()
+    {
+        // Act
+        var actual = Vnum.TryFromValue<TestVnum1>(3, out var vnum);
+
+        // Assert
+        Assert.False(actual);
+        Assert.Null(vnum);
+    }
+
+    [Fact]
+    public void FromEnum_GivenValidEnumValue_ShouldReturnExpectedResult()
+    {
+        // Act
+        var actual = Vnum.FromEnum<TestVnum, TestVnumId>(TestVnumId.OptionOne);
+
+        // Assert 
+        Assert.Equal(TestVnum.OptionOne, actual);
+    }
+
+    [Fact]
+    public void FromEnum_GivenInValidEnumValue_ShouldReturnExpectedResult()
+    {
+        // Act
+        var value = TestVnumId.OptionTwo;
+
+        // Assert
+        var ex = Assert.Throws<InvalidOperationException>(() => Vnum.FromEnum<TestVnum, TestVnumId>(value));
+        Assert.Equal($"'{(int)value}' is not a valid value in TestVnum", ex.Message);
+    }
+
+    [Fact]
+    public void TryFromEnum_GivenValidEnumValue_ShouldReturnTrue()
+    {
+        // Act
+        var actual = Vnum.TryFromEnum<TestVnum, TestVnumId>(TestVnumId.OptionOne, out var vnum);
+
+        // Assert
+        Assert.True(actual);
+        Assert.Equal(TestVnum.OptionOne, vnum);
+    }
+
+    [Fact]
+    public void TryFromEnum_GivenInvalidEnumValue_ShouldReturnFalse()
+    {
+        // Act
+        var actual = Vnum.TryFromEnum<TestVnum, TestVnumId>(TestVnumId.OptionTwo, out var vnum);
+
+        // Assert
+        Assert.False(actual);
+        Assert.Null(vnum);
+    }
+
+    [Fact]
+    public void FromCode_Given_Valid_Code_Should_Return_Correct_Vnum()
+    {
+        // Act
+        var actual = Vnum.FromCode<TestVnum1>("OptionOne");
+
+        // Assert
+        Assert.Equal(TestVnum1.OptionOne, actual);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("optionone")]
+    [InlineData("OPTIONONE")]
+    [InlineData("INVALID_CODE")]
+    public void FromCode_Given_Invalid_Code_Should_Throw_Exception(string code)
+    {
+        // Act
+        var ex = Assert.Throws<InvalidOperationException>(() => Vnum.FromCode<TestVnum1>(code));
+
+        // Assert
+        Assert.Equal($"'{code}' is not a valid code in TestVnum1", ex.Message);
+    }
+
+    [Theory]
+    [InlineData("optionone")]
+    [InlineData("OPTIONONE")]
+    public void FromCode_With_IgnoreCase_Should_Return_Correct_Vnum(string code)
+    {
+        // Act
+        var actual = Vnum.FromCode<TestVnum1>(code, ignoreCase: true);
+
+        // Assert
+        Assert.Equal(TestVnum1.OptionOne, actual);
+    }
+
+    [Fact]
+    public void TryFromCode_Given_Valid_Code_Should_Return_True_With_Expected_Vnum()
+    {
+        // Act
+        var actual = Vnum.TryFromCode<TestVnum1>("OptionOne", out var vnum);
+
+        // Assert
+        Assert.True(actual);
+        Assert.Equal(TestVnum1.OptionOne, vnum);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("optionone")]
+    [InlineData("OPTIONONE")]
+    [InlineData("INVALID_CODE")]
+    public void TryFromCode_Given_Invalid_Code_Should_Return_False_With_Null_Vnum(string code)
+    {
+        // Act
+        var actual = Vnum.TryFromCode<TestVnum1>(code, out var vnum);
+
+        // Assert
+        Assert.False(actual);
+        Assert.Null(vnum);
+    }
+
+    [Theory]
+    [InlineData("optionone")]
+    [InlineData("OPTIONONE")]
+    public void TryFromCode_With_IgnoreCase_Should_Return_Correct_Vnum(string code)
+    {
+        // Act
+        var result = Vnum.TryFromCode<TestVnum1>(code, ignoreCase: true, out var vnum);
+
+        // Assert
+        Assert.True(result);
+        Assert.Equal(TestVnum1.OptionOne, vnum);
+    }
+
+    //[Fact]
+    //public async Task GetAll_Should_Be_ThreadSafe()
+    //{
+    //    // Arrange: Clear the cache to ensure a clean state before the test.
+    //    Vnum.ClearCache();
+
+    //    // Act: This simulates a scenario where 100 threads attempt to access
+    //    // the Vnum instances simultaneously.
+    //    var tasks =
+    //        Enumerable.Range(0, 100).Select(_ =>
+    //            Task.Run(() =>
+    //            {
+    //                Vnum.GetAll<TestVnum1>();
+    //            }));
+
+    //    //Execute all tasks concurrently.
+
+    //    await Task.WhenAll(tasks);
+
+    //    //Task.WaitAll(tasks.ToArray());
+
+
+    //    //var exceptions = tasks.Where(t => t.IsFaulted).SelectMany(t => t.Exception.InnerExceptions).ToList();
+    //    //Assert.Empty(exceptions);
+
+    //    // Assert: The test ensures that the internal _cache dictionary, which
+    //    // is a shared resource, is thread-safe when accessed concurrently.
+    //    Assert.Single(Vnum._cache);
+    //    Assert.Equal(2, Vnum._cache[typeof(TestVnum1)].Length);
+    //}
+
+    [Fact]
+    public void GetAll_With_No_Predicate_Should_Return_All_Vnums()
+    {
+        // Act
+        var actual = Vnum.GetAll<TestVnum1>().ToList();
+
+        // Assert
+        var expected = new List<TestVnum1> { TestVnum1.OptionOne, TestVnum1.OptionTwo };
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void GetAll_With_Predicate_Filter_By_Value_Should_Return_Expected_Result()
+    {
+        // Act
+        var actual = Vnum.GetAll<TestVnum1>(x => x.Value == 1).ToList();
+
+        // Assert
+        Assert.Single(actual);
+        Assert.Equal(TestVnum1.OptionOne, actual[0]);
+    }
+
+    [Fact]
+    public void GetAll_With_Predicate_Filter_By_Code_Should_Return_Expected_Result()
+    {
+        // Act
+        var actual = Vnum.GetAll<TestVnum1>(x => x.Code == "OptionTwo").ToList();
+
+        // Assert
+        Assert.Single(actual);
+        Assert.Equal(TestVnum1.OptionTwo, actual[0]);
+    }
+
+    [Fact]
+    public void GetAll_With_No_Matching_Predicate_Should_Return_Empty_List()
+    {
+        // Act
+        var actual = Vnum.GetAll<TestVnum1>(x => x.Value == 9999).ToList();
+
+        // Assert
+        Assert.Empty(actual);
+    }
+
+    [Fact]
+    public void GetAll_Should_Exclude_Private_Vnums()
+    {
+        // Act
+        var actual = Vnum.GetAll<PrivateVnum>();
+
+        // Assert
+        Assert.Empty(actual);
+    }
+
+    [Fact]
+    public void GetAll_Should_Return_All_Vnum_Instances()
+    {
+        // Act
+        var actual = Vnum.GetAll(typeof(TestVnum1));
+
+        // Assert
+        var expected = new[] { TestVnum1.OptionOne, TestVnum1.OptionTwo };
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void GetAll_Given_Invalid_Type_Should_Throw_Exception()
+    {
+        // Act
+        var ex = Assert.Throws<ArgumentException>(() => Vnum.GetAll(typeof(NotVnum)));
+
+        // Assert
+        Assert.Equal("Type 'NotVnum' is not a valid Vnum type (Parameter 'type')", ex.Message);
+    }
+
+
+    //┌────────────────────────────────────────────────────────────────────┐
+    //│                             TEST DATA                              │
+    //└────────────────────────────────────────────────────────────────────┘
+
+    private sealed class NotVnum { }
+
+    private enum TestVnumId
+    {
+        OptionOne = 1,
+        OptionTwo = 2
+    }
+
+    private sealed class TestVnum : Vnum<TestVnumId>
+    {
+        public TestVnum() { }
+        private TestVnum(TestVnumId value, string code) : base(value, code) { }
+
+        public static readonly TestVnum OptionOne = new TestVnum(TestVnumId.OptionOne, "OptionOne");
+
+        //OptionTwo intentionally not implemented 
+    }
+
+    private sealed class TestVnum1 : Vnum
+    {
+        public static readonly TestVnum1 OptionOne = new TestVnum1(1, "OptionOne", "OptionOne_Description");
+        public static readonly TestVnum1 OptionTwo = new TestVnum1(2, "OptionTwo", "OptionTwo_Description");
+
+        public string CustomDescription { get; } = null!;
+        private string PrivateProp { get; } = "private";
+        internal string InternalProp { get; } = "internal";
+
+        public TestVnum1() { }
+        private TestVnum1(int value, string code, string customDescription) : base(value, code)
+        {
+            CustomDescription = customDescription;
+        }
+
+        public static TestVnum1 Stub(int value, string code) => new TestVnum1(value, code, customDescription: string.Empty);
+    }
+
+    private sealed class TestVnum2 : Vnum
+    {
+        public TestVnum2() { }
+        private TestVnum2(int value, string code) : base(value, code) { }
+    }
+
+    private sealed class TestVnum3 : Vnum
+    {
+        public static readonly TestVnum3 OptionOne = new TestVnum3(1, "OptionOne");
+
+        public TestVnum3() { }
+        private TestVnum3(int value, string code) : base(value, code)
+        {
+        }
+    }
+
+    private sealed class PrivateVnum : Vnum
+    {
+        private static readonly PrivateVnum HiddenOption = new PrivateVnum(1, "HiddenOption");
+
+        public PrivateVnum() { }
+        private PrivateVnum(int value, string code) : base(value, code) { }
+    }
+}
